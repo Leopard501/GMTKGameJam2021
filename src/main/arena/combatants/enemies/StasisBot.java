63eac001732a13f05fa25ec21ded8c32ab480b23
Leopard501 <leopard50x1@gmail.com>
2021-06-11 19:30:19 -0700

    added StasisBot

---
+++ src/main/arena/combatants/enemies/StasisBot.java
@@ -0,0 +1,20 @@
+package main.arena.combatants.enemies;
+
+import main.arena.buffs.Sticky;
+import main.arena.combatants.Combatant;
+import main.arena.combatants.abilities.OffensiveAbility;
+import processing.core.PApplet;
+
+import java.awt.*;
+
+public class StasisBot extends Combatant implements OffensiveAbility {
+
+    public StasisBot(PApplet p) {
+        super(p, 40, 10, 5, 8, 0, new Color(0, 125, 255));
+    }
+
+    @Override
+    public void ability(Combatant other) {
+        other.sticky = new Sticky(P);
+    }
+}
